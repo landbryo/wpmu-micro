a1b50fb2015488b72a323f47fbbbe9d0f553c2c3
Abdul Wahab <meetawahab@gmail.com>
2020-10-31 20:07:43 +0500

    Apply WP coding standard and set prefix of method.

--- wpmu-micro.php
+++ wpmu-micro.php
@@ -1,12 +1,12 @@
 <?php
 
-add_action('wpmu_new_blog', 'landbryo_create_pages', 10, 2);
+add_action( 'wpmu_new_blog', 'landbryo_create_pages', 10, 2 );
 
-function landbryo_create_pages($blog_id, $user_id){
-  switch_to_blog($blog_id);
+function landbryo_create_pages( $blog_id, $user_id ){
+  switch_to_blog( $blog_id );
 
-// create home page
-  $page_id = wp_insert_post(array(
+	// Create home page.
+  $page_id = wp_insert_post( array(
     'post_title'     => 'Home',
     'post_name'      => 'home',
     'post_content'   => '',
@@ -17,10 +17,10 @@ function landbryo_create_pages($blog_id, $user_id){
     'comment_status' => 'closed',
     'ping_status'    => 'closed',
     'page_template'  => 'page-home.php',
-  ));
+  ) );
 
-// create contact page
-  $page_id = wp_insert_post(array(
+	// Create contact page.
+  $page_id = wp_insert_post( array(
     'post_title'     => 'Contact',
     'post_name'      => 'contact',
     'post_content'   => '[gravityform id="1" title="false" description="false"]',
@@ -31,26 +31,27 @@ function landbryo_create_pages($blog_id, $user_id){
     'comment_status' => 'closed',
     'ping_status'    => 'closed',
     'page_template'  => '',
- ));
+ 	) );
 
-// Find and delete the WP default post and page
-    $defaultPage = get_page_by_title( 'Sample Page' );
-    $defaultPost = get_page_by_title( 'Hello world!' );
-    wp_delete_post( $defaultPage->ID );
-    wp_delete_post( $defaultPost->ID );
+	// Find and delete the WP default post and page.
+	$defaultPage = get_page_by_title( 'Sample Page' );
+	$defaultPost = get_page_by_title( 'Hello world!' );
+	wp_delete_post( $defaultPage->ID );
+	wp_delete_post( $defaultPost->ID );
 
-// Set default home page
-    $home = get_page_by_title( 'Home' );
-    update_option( 'page_on_front', $home->ID );
-    update_option( 'show_on_front', 'page' );
+	// Set default home page.
+	$home = get_page_by_title( 'Home' );
+	update_option( 'page_on_front', $home->ID );
+	update_option( 'show_on_front', 'page' );
 
-  // Leave this line at the end of this function
+  // Leave this line at the end of this function to restore the current blog.
   restore_current_blog();
 }
 
-// set permalink structure
-function set_permalinks() {
-    global $wp_rewrite;
-    $wp_rewrite->set_permalink_structure( '/%postname%/' );
+// Set permalink structure on the creation of new site.
+function landbryo_set_permalinks() {
+	global $wp_rewrite;
+	$wp_rewrite->set_permalink_structure( '/%postname%/' );
 }
-add_action( 'wpmu_new_blog', 'set_permalinks' );
+
+add_action( 'wpmu_new_blog', 'landbryo_set_permalinks' );
